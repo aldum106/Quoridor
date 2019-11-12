150053bcbcee8fd665138ad6c88cb8ac1eb0efb6
Marc-André Guy <markguy3@hotmail.fr>
2019-11-11 20:58:50 -0500

    1ere fonction jouer pour jouer une partie

--- main.py
+++ main.py
@@ -43,19 +43,19 @@ def afficher_damier_ascii(dic):
 from api import *
 
 
-def jouer_a_lejeu():
+def jouer():
     idul = input('Quel est ton nom de joueur? ')
     etat = débuter_partie(idul)[1]
     identif = débuter_partie(idul)[0]
 
     while True:
         afficher_damier_ascii(etat)
-        type_coup = input('Veux-tu jouer un mur ou un déplacement ? (entrez D, MH ou MV) ')
+        type_coup = input('Quel est ton coup (D, MH ou MV) ?')
         position_x = int(input('Choisis une case en x :'))
         position_y = int(input('Choisis une case en y :'))
         etat = jouer_coup(identif, type_coup, (position_x, position_y))
         if 'gagnant' in etat.keys():
              continue
-jouer_a_lejeu()
 
 
+jouer()
