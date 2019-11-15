f90abd2d7da21175a73083ee6c646ef8c010cc65
Marc-André Guy <markguy3@hotmail.fr>
2019-11-15 15:20:19 -0500

    pep8

--- api.py
+++ api.py
@@ -1,37 +1,37 @@
 import requests
 
 
-url = 'https://python.gel.ulaval.ca/quoridor/api/'
+u = 'https://python.gel.ulaval.ca/quoridor/api/'
 
 
 def lister_parties(idul):
-    rep = requests.get(url+'lister/', params={'idul': idul})
+    rep = requests.get(u+'lister/', params={'idul': idul})
     if rep.status_code == 200:
-         rep = rep.json()
-         if 'message' in rep.keys():
-              raise RuntimeError(rep['message'])
-         return rep['parties']
-    print(f"Le GET sur {url+'lister/'} a produit le code d'erreur {rep.status_code}.")
+        rep = rep.json()
+        if 'message' in rep.keys():
+            raise RuntimeError(rep['message'])
+        return rep['parties']
+    print(f"Le GET sur {u+'lister/'} a produit le code d'erreur {rep.status_code}.")
 
 
 def débuter_partie(idul):
-    rep = requests.post(url+'débuter/', data={'idul': idul})
+    rep = requests.post(u+'débuter/', data={'idul': idul})
     if rep.status_code == 200:
-         rep = rep.json()
-         if 'message' in rep.keys():
-              raise RuntimeError(rep['message'])
-         return (rep['id'], rep['état'])
-    print(f"Le POST sur {url+'débuter/'} a produit le code d'erreur {rep.status_code}.")
+        rep = rep.json()
+        if 'message' in rep.keys():
+            raise RuntimeError(rep['message'])
+        return (rep['id'], rep['état'])
+    print(f"Le POST sur {u+'débuter/'} a produit le code d'erreur {rep.status_code}.")
 
 
 def jouer_coup(id_partie, type_coup, position):
-    rep = requests.post(url+'jouer/', data={'id': id_partie, 'type' : type_coup,
-    'pos' : position})
+    rep = requests.post(u+'jouer/', data={'id': id_partie, 'type' : type_coup,
+                        'pos' : position})
     if rep.status_code == 200:
-         rep = rep.json()
-         if 'gagnant' in rep.keys():
-              raise StopIteration(rep['gagnant'])
-         if 'message' in rep.keys():
-              raise RuntimeError(rep['message'])
-         return rep['état']
-    print(f"Le POST sur {url+'jouer/'} a produit le code d'erreur {rep.status_code}.")
+        rep = rep.json()
+        if 'gagnant' in rep.keys():
+            raise StopIteration(rep['gagnant'])
+        if 'message' in rep.keys():
+            raise RuntimeError(rep['message'])
+        return rep['état']
+    print(f"Le POST sur {u+'jouer/'} a produit le code d'erreur {rep.status_code}.")
